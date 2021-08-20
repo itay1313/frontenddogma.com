efa9b89a0d7b7e3bf76e08e4bc290d7f1352a8ba
Jens Oliver Meiert <jens@meiert.com>
2021-08-20 12:46:28 +0200

    Corrected timezone to CET
    Signed-off-by: Jens Oliver Meiert <jens@meiert.com>

--- .eleventy.js
+++ .eleventy.js
@@ -19,12 +19,12 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addLayoutAlias("post", "post.njk");
 
   eleventyConfig.addFilter("readableDate", dateObj => {
-    return DateTime.fromJSDate(dateObj, {zone: "utc"}).toFormat("MMMM d, yyyy");
+    return DateTime.fromJSDate(dateObj, {zone: "utc+1"}).toFormat("MMMM d, yyyy");
   });
 
   // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
   eleventyConfig.addFilter("htmlDateString", (dateObj) => {
-    return DateTime.fromJSDate(dateObj, {zone: "utc"}).toFormat("yyyy-LL-dd");
+    return DateTime.fromJSDate(dateObj, {zone: "utc+1"}).toFormat("yyyy-LL-dd");
   });
 
   // Get the first `n` elements of a collection.
